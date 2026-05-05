f0a84ce6581747c82a4c6b86b88162a537ecdd6b
Priyankaa2503 <priyankaar25@gmail.com>
2026-05-04 21:59:36 -0700

    view 3

--- Homework3/priramachandran/src/App.jsx
+++ Homework3/priramachandran/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import RenderOptions, { DEFAULT_STOCK } from "./component/options";
 import { StockLineChart } from "./component/LineChart";
+import { TSNEScatter } from "./component/TSNEScatter";
+import { NewsList } from "./component/NewsList";
 
 function App() {
   const [selectedStock, setSelectedStock] = useState(DEFAULT_STOCK);
@@ -33,19 +35,19 @@ function App() {
           </div>
           <div className="flex flex-1 min-h-0 flex-col p-2">
             <h3 className="text-left text-xl h-8 shrink-0">
-              View 2 to be replaced by the view title
+            T-SNE Scatter Plot
             </h3>
-            <div className="border-2 border-gray-300 rounded-xl flex-1 min-h-0 h-[calc(100%_-_2rem)]">
-              <p className="text-center text-gray-500 mt-20">Empty View 2</p>
+            <div className="border-2 border-gray-300 rounded-xl flex-1 min-h-0 h-[calc(100%_-_2rem)] overflow-hidden flex flex-col">
+              <TSNEScatter selectedTicker={selectedStock} />
             </div>
           </div>
         </div>
-        <div className="w-1/3 h-full p-2">
+        <div className="w-1/3 h-full p-2 min-h-0">
           <h3 className="text-left text-xl h-8">
-            View 3 to be replaced by the view title
+          List of News
           </h3>
-          <div className="border-2 border-gray-300 rounded-xl h-[calc(100%-2rem)]">
-            <p className="text-center text-gray-500 mt-20">Empty View 3</p>
+          <div className="border-2 border-gray-300 rounded-xl h-[calc(100%_-_2rem)] min-h-0">
+            <NewsList ticker={selectedStock} />
           </div>
         </div>
       </div>
