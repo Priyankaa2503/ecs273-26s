c442405ef991d5801c26cd278fdd80f329657dcf
Priyankaa2503 <priyankaar25@gmail.com>
2026-05-04 21:33:35 -0700

    dropdown and view 1

--- Homework3/priramachandran/src/component/options.jsx
+++ Homework3/priramachandran/src/component/options.jsx
@@ -1,10 +1,32 @@
-import Data from "../../data/demo.json";
+export const STOCKS = [
+  "AAPL",
+  "BAC",
+  "CAT",
+  "CVX",
+  "DAL",
+  "GOOGL",
+  "GS",
+  "HAL",
+  "JNJ",
+  "JPM",
+  "KO",
+  "MCD",
+  "META",
+  "MMM",
+  "MSFT",
+  "NKE",
+  "NVDA",
+  "PFE",
+  "UNH",
+  "XOM",
+];
+
+export const DEFAULT_STOCK = "AAPL";
 
 export default function RenderOptions() {
-    const bars = Data.data;
-    return bars.map((bar, index) => (
-      <option key={index} value={bar.category}>
-        {bar.category}
-      </option>
-    ));
-  }
+  return STOCKS.map((symbol) => (
+    <option key={symbol} value={symbol}>
+      {symbol}
+    </option>
+  ));
+}
